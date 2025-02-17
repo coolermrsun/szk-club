83b466d68d9bb4f9b42955d22489f5de32081413
coolermrsun <2584464580@qq.com>
2025-02-17 21:08:26 +0600

    工厂+策略

---
+++ szk-club-subject/szk-club-domain/target/generated-sources/annotations/com/szk/subject/domain/convert/BriefSubjectConverterImpl.java
@@ -0,0 +1,27 @@
+package com.szk.subject.domain.convert;
+
+import com.szk.subject.domain.entity.SubjectInfoBO;
+import com.szk.subject.infra.basic.entity.SubjectBrief;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-02-17T21:06:57+0600",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_432 (Amazon.com Inc.)"
+)
+public class BriefSubjectConverterImpl implements BriefSubjectConverter {
+
+    @Override
+    public SubjectBrief convertBoToEntity(SubjectInfoBO subjectInfoBO) {
+        if ( subjectInfoBO == null ) {
+            return null;
+        }
+
+        SubjectBrief subjectBrief = new SubjectBrief();
+
+        subjectBrief.setId( subjectInfoBO.getId() );
+        subjectBrief.setSubjectAnswer( subjectInfoBO.getSubjectAnswer() );
+
+        return subjectBrief;
+    }
+}
